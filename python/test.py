4d2f4e1937fc5e1ca56c4c56ed39fc794bc2e656
Abhi Jagdev <abhijagdev@gmail.com>
2016-11-28 12:08:58 -0800

    test file cleanup

--- python/test.py
+++ python/test.py
@@ -4,18 +4,20 @@ import unittest
 import time
 
 
-from p3 import triggers
+import p3
 
 class TestClass(unittest.TestCase):
 
-    def test_triggers(self):
+
+	# check if sensors are triggered in p3
+	def test_triggers(self):
 
 		EW_ped_button = Value('b', False)
 		NS_ped_button = Value('b', False)
 		EW_sensor = Value('b', False)
 		NS_sensor = Value('b', False)
 
-		p = Process(target=triggers, args=(EW_ped_button, NS_ped_button, EW_sensor, NS_sensor))
+		p = Process(target=p3.triggers, args=(EW_ped_button, NS_ped_button, EW_sensor, NS_sensor))
 		p.daemon = True
 		p.start()
 
@@ -25,7 +27,7 @@ class TestClass(unittest.TestCase):
 		self.assertTrue(NS_ped_button.value)
 		self.assertTrue(EW_sensor.value)
 		self.assertTrue(NS_sensor.value)
-
+		
 if __name__ == '__main__':
 	unittest.main()
 	
